e24b205a64535c00c6f4748f7fa20a5ba27a2911
Unknown <ludovic.marchand@hotmail.be>
2018-01-18 19:51:38 +0100

    Add room w/ test object

--- ARMonitoring/Controller/ARViewController.swift
+++ ARMonitoring/Controller/ARViewController.swift
@@ -17,6 +17,8 @@ class ARViewController: UIViewController, ARSCNViewDelegate, StompClientDelegate
     
     private var stompClient: StompClientService?
     
+    // TODO: make delegate for room detection (Beacons)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,7 +29,8 @@ class ARViewController: UIViewController, ARSCNViewDelegate, StompClientDelegate
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        // let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -58,6 +61,21 @@ class ARViewController: UIViewController, ARSCNViewDelegate, StompClientDelegate
         // Release any cached data, images, etc that aren't in use.
     }
     
+    // MARK: - Buttons
+    
+    @IBAction func addItemButton(_ sender: Any) {
+        let testItem = ItemNode(withName: "add_test_1")
+        let cc = sceneView.getCameraCoordinates()
+        
+        testItem.position = SCNVector3(cc.x, cc.y, cc.z-1)
+        
+        // Add testItem to RoomNode
+        // RoomNode needs to be add programmatically by beacon position
+        // sceneView.getNode(name: "RoomNode")?.addChildNode(testItem)
+        
+        sceneView.scene.rootNode.addChildNode(testItem)
+    }
+    
     // MARK: - ARSCNViewDelegate
     
     /*
@@ -69,7 +87,6 @@ class ARViewController: UIViewController, ARSCNViewDelegate, StompClientDelegate
      }
      */
     
-    // ARSCN delegate
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
         
@@ -85,20 +102,49 @@ class ARViewController: UIViewController, ARSCNViewDelegate, StompClientDelegate
         
     }
     
-    // WebSockets
-    func startWebsocket() -> () {
-        self.stompClient = StompClientService()
-        self.stompClient?.delegate = self
+    // MARK: - RoomShizzle
+    //tmp function to add testRoom
+    @IBAction func resetRoomButton(_ sender: Any) {
+        generateRoom()
+    }
+    
+    func generateRoom() {
+        if let node = sceneView.getNode(name: "RoomNode") {
+            node.removeFromParentNode()
+        }
+        // TODO: Set room at coordinates with beacons
+        //tmp fix
+        let roomNode: SCNNode = SCNNode()
+        roomNode.position = sceneView.getCameraCoordinates()
+        roomNode.name = "RoomNode"
         
-        self.stompClient?.openSocket(withUrl: URL(string: "http://10.3.50.6:80/managerWS/websocket")!)
+        // TODO: Itterate over 'Room' model to add items at correct coordinates inside the Room
+        let roomJSON = "{\"id\": \"room_test_id\", \"name\": \"test_room\", \"description\": \"where is the room located in the building, ex A.2.204\", \"item_list\": [{ \"id\": \"test_item\", \"name\": \"HDD Server Rack 312\", \"location\": { \"x\": 0.0, \"y\": 0.0, \"z\": 10.0 }, \"row_list\": [\"first_row\", \"second_row\", \"third_row\"], \"room_id\": \"room_test_id\"}]}"
+        let room: Room = Room(JSONString: roomJSON)!
+        for item: Item in room.item_list {
+            roomNode.addChildNode(ItemNode(withItem: item))
+        }
+        
+        sceneView.scene.rootNode.addChildNode(roomNode)
     }
     
-    // WebSockets delegate
-    func didReceiveJSON(dataLog: DataLog) {
+    // MARK: - WebSockets & StompClientDelegate
+    func startWebsocket() -> () {
+        let url = URL(string: "http://10.3.50.6:80/managerWS/websocket")!
+        
+        self.stompClient = StompClientService(delegate: self, socketUrl: url)
+        self.stompClient?.openSocket()
+    }
+    
+    func stompDidReceiveJSON(dataLog: DataLog) {
         print(dataLog.toLog())
+        
+        if let node = sceneView.getNode(name: dataLog.item_id) {
+            // Edit node
+        }
     }
     
-    func test(text: String) {
+    func stompTest(text: String) {
         print(text)
     }
 }
